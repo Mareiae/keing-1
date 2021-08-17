6cca14e2501a0ab676900532b46685c32017620c
hxoreyer <1067374087@qq.com>
2021-08-17 17:29:59 +0800

    go fmt

--- logger.go
+++ logger.go
@@ -5,12 +5,12 @@ import (
 	"time"
 )
 
-func Logger() HandlerFunc{
+func Logger() HandlerFunc {
 	return func(c *Context) {
 		t := time.Now()
 		c.Next()
-		ts := fmt.Sprintf("%d/%d/%d - %d:%d:%d",time.Now().Year(),time.Now().Month(),time.Now().Day(),time.Now().Hour(),time.Now().Minute(),time.Now().Second())
-		fmt.Printf("[Keing] %-22s| %3d | %15s | %16s | %6s  \"%-s\"\n",ts,c.code,time.Since(t),c.Request.RemoteAddr,c.Request.Method,c.Request.RequestURI)
+		ts := fmt.Sprintf("%d/%d/%d - %d:%d:%d", time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second())
+		fmt.Printf("[Keing] %-22s| %3d | %15s | %16s | %6s  \"%-s\"\n", ts, c.code, time.Since(t), c.Request.RemoteAddr, c.Request.Method, c.Request.RequestURI)
 		if len(c.Errors) > 0 {
 			fmt.Println(c.Errors)
 		}
--- path.go
+++ path.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Path struct {
-	method 	string
-	path 	string
+	method string
+	path   string
 }
 
 type Paths struct {
-	maxNum	int
-	paths 	map[int]*Path
+	maxNum int
+	paths  map[int]*Path
 }
 
-func (p *Paths)AddPath(method, path string, n int){
+func (p *Paths) AddPath(method, path string, n int) {
 	p.paths[n] = &Path{
 		method: method,
 		path:   path,
@@ -23,13 +23,13 @@ func (p *Paths)AddPath(method, path string, n int){
 	p.maxNum++
 }
 
-func (p *Paths)getPathString(n int)string{
-	str := fmt.Sprintf("--> func%d (%d handlers)",n,p.maxNum)
-	s := fmt.Sprintf("[Keing] %-"+strconv.Itoa(20)+"s%-"+strconv.Itoa(20)+"s%-"+strconv.Itoa(20)+"s",p.paths[n].method,p.paths[n].path,str)
+func (p *Paths) getPathString(n int) string {
+	str := fmt.Sprintf("--> func%d (%d handlers)", n, p.maxNum)
+	s := fmt.Sprintf("[Keing] %-"+strconv.Itoa(20)+"s%-"+strconv.Itoa(20)+"s%-"+strconv.Itoa(20)+"s", p.paths[n].method, p.paths[n].path, str)
 	return s
 }
-func (p *Paths)ShowAllPathString(){
-	for k,_ := range p.paths{
+func (p *Paths) ShowAllPathString() {
+	for k, _ := range p.paths {
 		fmt.Println(p.getPathString(k))
 	}
 }
